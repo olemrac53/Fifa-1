83800840ca4ad00758cb76f907534835ce6de1ff
Lab20-PC02 <Lab20-PC02@L20-03>
2025-08-26 20:01:37 +0200

    Agregar archivos de proyecto.

---
+++ Fifa-1/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Fifa_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            dataGridView3 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            dataGridView2 = new DataGridView();
+            button3 = new Button();
+            dataGridView4 = new DataGridView();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView4).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView3
+            // 
+            dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView3.Location = new Point(186, 367);
+            dataGridView3.Name = "dataGridView3";
+            dataGridView3.Size = new Size(93, 55);
+            dataGridView3.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(195, 384);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 11;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(371, 384);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 13;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(362, 367);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(93, 55);
+            dataGridView2.TabIndex = 12;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(548, 384);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 15;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView4
+            // 
+            dataGridView4.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView4.Location = new Point(539, 367);
+            dataGridView4.Name = "dataGridView4";
+            dataGridView4.Size = new Size(93, 55);
+            dataGridView4.TabIndex = 14;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(371, 105);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 17;
+            label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(791, 450);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(dataGridView4);
+            Controls.Add(button2);
+            Controls.Add(dataGridView2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView3);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private DataGridView dataGridView3;
+        private Button button1;
+        private Button button2;
+        private DataGridView dataGridView2;
+        private Button button3;
+        private DataGridView dataGridView4;
+        private Label label1;
+    }
+}
+
+
+
+
+
