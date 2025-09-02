cbf3d79bf8988ac6bb53beaa3dfd09f3cea708e5
torren007 <torrenprietoruben@gmail.com>
2025-09-02 18:47:20 +0200

    inicio sesion W.I.P
    Trabajo en progreso de inicio sesion del login

---
+++ Fifa-1/Inicio sesion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fifa_1
+{
+    public partial class Inicio_sesion : Form
+    {
+        public Inicio_sesion()
+        {
+            InitializeComponent();
+        }
+    }
+}
