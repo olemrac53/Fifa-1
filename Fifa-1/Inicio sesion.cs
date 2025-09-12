2d64e8a57bc5a03569a065a5b7edf3b26a4d7a2a
torren007 <torrenprietoruben@gmail.com>
2025-09-12 10:32:25 -0300

    wea
    wa

--- Fifa-1/Inicio sesion.cs
+++ Fifa-1/Inicio sesion.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
+﻿
 namespace Fifa_1
 {
-    public partial class Inicio_sesion : Form
+    public partial class Inicio_sesion : Form1
     {
         public Inicio_sesion()
         {
--- Triggers.sql
+++ Triggers.sql
@@ -19,3 +19,36 @@ DELIMITER ;
 
 
 SELECT PuntajePlantillaFecha(1, 4);
+
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS TR_ValidarPuntuacionUnica $$
+CREATE TRIGGER TR_ValidarPuntuacionUnica
+BEFORE INSERT ON PuntuacionFutbolista
+FOR EACH ROW
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM PuntuacionFutbolista pf
+        WHERE pf.id_futbolista = NEW.id_futbolista
+          AND pf.fecha = NEW.fecha
+    ) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'El futbolista ya tiene una puntuación para esa fecha.';
+    END IF;
+END $$
+DELIMITER ;
+
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS TR_ValidarTipoFutbolista $$
+CREATE TRIGGER TR_ValidarTipoFutbolista
+BEFORE INSERT ON Futbolista
+FOR EACH ROW
+BEGIN
+    IF NEW.id_tipo IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Todo futbolista debe tener asignado un tipo válido (Arquero, Defensor, etc).';
+    END IF;
+END $$
+DELIMITER ;
