11c0ea819db2215ae32e4b4998712cfeb983910e
torren007 <torrenprietoruben@gmail.com>
2025-09-12 10:34:16 -0300

    Triggers
    Self explanotry

--- Triggers.sql
+++ Triggers.sql
@@ -52,3 +52,66 @@ BEGIN
     END IF;
 END $$
 DELIMITER ;
+
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS TR_ValidarPresupuestoPlantilla $$
+CREATE TRIGGER TR_ValidarPresupuestoPlantilla
+BEFORE INSERT ON PlantillaFutbolista
+FOR EACH ROW
+BEGIN
+    DECLARE total DECIMAL(11,2);
+
+    -- Calcular el presupuesto actual de la plantilla
+    SELECT COALESCE(SUM(f.cotizacion),0)
+    INTO total
+    FROM PlantillaFutbolista pf
+    JOIN Futbolista f ON f.id_futbolista = pf.id_futbolista
+    WHERE pf.id_plantilla = NEW.id_plantilla;
+
+    -- Validar que no supere el máximo
+    IF (total + (SELECT cotizacion FROM Futbolista WHERE id_futbolista = NEW.id_futbolista)) > 99999999.99 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'El presupuesto de la plantilla no puede superar los $99.999.999,99.';
+    END IF;
+END $$
+DELIMITER ;
+
+
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS TR_ValidarCantidadPlantilla $$
+CREATE TRIGGER TR_ValidarCantidadPlantilla
+BEFORE INSERT ON PlantillaFutbolista
+FOR EACH ROW
+BEGIN
+    DECLARE cantidad INT;
+
+    SELECT COUNT(*) INTO cantidad
+    FROM PlantillaFutbolista
+    WHERE id_plantilla = NEW.id_plantilla;
+
+    IF cantidad >= 20 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'No se pueden agregar más de 20 jugadores en la plantilla.';
+    END IF;
+END $$
+DELIMITER ;
+
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS TR_ValidarEmailUsuario $$
+CREATE TRIGGER TR_ValidarEmailUsuario
+BEFORE INSERT ON Usuario
+FOR EACH ROW
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM Usuario u
+        WHERE u.email = NEW.email
+    ) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'El email ya está registrado.';
+    END IF;
+END $$
+DELIMITER ;
