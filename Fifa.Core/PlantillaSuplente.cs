67152e3998a140157e7eb729a1fbe6c26e258194
torren007 <torrenprietoruben@gmail.com>
2025-10-23 10:29:36 -0300

    Se neutralizo esto tambien
    para que le haga compañia a los otros desaparecidos

--- Fifa.Core/PlantillaSuplente.cs
+++ Fifa.Core/PlantillaSuplente.cs
@@ -2,7 +2,6 @@
 {
     public class PlantillaSuplente
     {
-        public required int idPlantillaSuplente { get; set; }
         public required Plantilla Plantilla { get; set; }
         public required Futbolista Futbolista { get; set; }
     }
