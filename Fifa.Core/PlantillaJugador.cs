b214ec320c3e0144c9d7aa09fe590ed0f8d74c04
olemrac53 <gonzalezcarmelo903@gmail.com>
2025-10-28 16:06:06 -0300

    wip

--- Fifa.Core/PlantillaJugador.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fifa.Core
-{
-    public class PlantillaJugador
-    {
-        public required int idPlantillaJugador { get; set; }
-        public required Plantilla Plantilla { get; set; }
-        public required Futbolista Futbolista { get; set; }
-    }
-}
---
+++ ReposDapperTests/TestRepoUsuario.cs
@@ -0,0 +1,15 @@
+using Fifa.Core;
+using Fifa.Core.Repos;
+using Fifa.Dapper;
+using super.Test;
+
+namespace ReposDapperTests;
+
+public class TestRepoUsuario : TestRepo
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
