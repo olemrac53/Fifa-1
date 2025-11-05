ca91d25ca09ba6978f46b12d50734c0edbe44c9a
torren007 <torrenprietoruben@gmail.com>
2025-11-05 11:59:27 -0300

    mbn

---
+++ ReposDapperTests/TestRepoPuntuacion.cs
@@ -0,0 +1,12 @@
+using Fifa.Core;
+using Fifa.Core.Repos;
+using Fifa.Dapper;
+using Dapper;
+using MySqlConnector;
+
+namespace Fifa.Test;
+
+public class TestRepoPuntuacion : TestRepo, IDisposable
+{
+    
+}
